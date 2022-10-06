6173ee5d6c568a50a1e27623bcde25c47a70861c
aleksey-kod <aleksey.kod@gmail.com>
2022-10-06 10:49:59 +0300

    Домашнее задание Lesson2

---
+++ src/main/java/Lesson_two_homework/SortAnArray.java
@@ -0,0 +1,45 @@
+package Lesson_two_homework;
+
+public class SortAnArray {
+    public static int[] sortArray(int[] arrayA) {
+        // проверяем не нулевой ли он?
+        if (arrayA == null) {
+            return null;
+        }
+        // проверяем не 1 ли элемент в массиве?
+        if (arrayA.length < 2) {
+            return arrayA;
+        }
+        // копируем левую часть от начала до середины
+        int[] arrayB = new int[arrayA.length / 2];
+        System.arraycopy(arrayA, 0, arrayB, 0, arrayA.length / 2);
+        // копируем правую часть от середины до конца массива, вычитаем из длины первую часть
+        int[] arrayC = new int[arrayA.length - arrayA.length / 2];
+        System.arraycopy(arrayA, arrayA.length / 2, arrayC, 0, arrayA.length - arrayA.length / 2);
+        arrayB = sortArray(arrayB); // левая часть возврат из рекурсии строкой return arrayA;
+        arrayC = sortArray(arrayC); // правая часть возврат из рекурсии строкой return arrayA;
+        // далее опять рекурсия возврата слияния двух отсортированных массивов
+        return mergeArray(arrayB, arrayC);
+    }
+    public static int[] mergeArray(int[] arrayA, int[] arrayB) {
+        int[] arrayC = new int[arrayA.length + arrayB.length];
+        int positionA = 0, positionB = 0;
+        for (int i = 0; i < arrayC.length; i++) {
+            if (positionA == arrayA.length) {
+                arrayC[i] = arrayB[positionB];
+                positionB++;
+            } else if (positionB == arrayB.length) {
+                arrayC[i] = arrayA[positionA];
+                positionA++;
+            } else if (arrayA[positionA] < arrayB[positionB]) {
+                arrayC[i] = arrayA[positionA];
+                positionA++;
+            } else {
+                arrayC[i] = arrayB[positionB];
+                positionB++;
+            }
+        }
+        return arrayC;
+    }
+
+}
